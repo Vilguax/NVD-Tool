82f940bd7ca03bf6952305074798c3c0fc4931e8
Vilguax <axel.pelassa@hotmail.fr>
2023-03-13 09:31:29 +0100

    test

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
 #test.py
 #Fichier de test pour l'ajout de fonctionnalités au projet
-
-import nvdlib as nvd
-import datetime
-
-end = datetime.datetime.now()
-last4h = end - datetime.timedelta(days=30)
-
-r = nvd.searchCVE(lastModStartDate=last4h, lastModEndDate=end)
-c = 0
-for i in r:
-    c += 1
-    
-print('Nombre de CVEs: ', c)
